1317926583d964d8b6bfd12fb984dacf8bf26034
Kamil Purgat <kpurgat.zsp9@gmail.com>
2022-11-23 10:05:48 +0100

    Adding avr libraries

---
+++ 23.11/23.11/main.c
@@ -0,0 +1,34 @@
+/*
+ * 23.11.c
+ *
+ * Created: 23.11.2022 09:07:15
+ * Author : kamil
+ */
+
+/*Praca do domu: 
+sprawdziæ wielkoœæ:
+ char
+ int
+ float
+ double
+ unsigned int
+ long int
+ long long int
+ 
+ sprawdzic ile taktow zajmuje dodawanie dwóch takich samych typow i kiedy jest najdluzej 
+*/
+
+
+#include <avr/io.h>
+#include "global.h"
+#include "spi.h"
+#include "i2c"
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
